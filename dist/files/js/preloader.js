874a952ec6d2ad10884c79789f15d7209e60dfda
Oleksii S. <asallweb@gmail.com>
2025-01-14 10:25:43 +0100

    v2.62

--- dist/files/js/preloader.js
+++ dist/files/js/preloader.js
@@ -2,12 +2,13 @@ document.addEventListener("DOMContentLoaded", (function() {
     document.documentElement.classList.add("lock");
     const wrapper = document.getElementById("wrapper");
     const header = document.getElementById("header");
+    const heroImage = document.querySelector(".hero__static-bg");
     function hidePreloader() {
-        setTimeout((() => {
-            document.documentElement.classList.remove("lock");
-            if (wrapper) wrapper.classList.add("_wrapper-active");
-            if (header) header.classList.add("_active");
-        }), 210);
+        document.documentElement.classList.remove("lock");
+        if (wrapper) wrapper.classList.add("_wrapper-active");
+        if (header) header.classList.add("_active");
     }
-    window.onload = hidePreloader;
+    if (heroImage) if (heroImage.complete) setTimeout(hidePreloader, 100); else heroImage.addEventListener("load", (function() {
+        setTimeout(hidePreloader, 100);
+    })); else setTimeout(hidePreloader, 100);
 }));
